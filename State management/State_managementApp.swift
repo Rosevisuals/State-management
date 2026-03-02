f319093fd2ccc2eac24ea8d9076061fa236217e7
Namubiru Rose <145221322+Rosevisuals@users.noreply.github.com>
2026-03-02 07:47:29 +0300

    Initial Commit

---
+++ State management/State_managementApp.swift
@@ -0,0 +1,17 @@
+//
+//  State_managementApp.swift
+//  State management
+//
+//  Created by Rose Visuals on 02/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct State_managementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
